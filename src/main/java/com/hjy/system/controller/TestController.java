d88dd5adf03d0ea3e13dd507cd79c5fbfac04d5f
lc4260 <e8ukaw@163.com>
2020-12-04 12:23:54 +0800

    git小号提交测试

---
+++ src/main/java/com/hjy/system/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.hjy.system.controller;
+
+/**
+ * @author liuchun
+ * @Package com.hjy.system.controller
+ * @date 2020/12/4 12:22
+ * description:
+ */
+public class TestController {
+}
